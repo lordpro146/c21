05bc99af2e36cfa86b9162bc21e1422f2fe76b29
lordpro146 <71271084+lordpro146@users.noreply.github.com>
2020-09-18 20:53:29 +0530

    Add files via upload

---
+++ bounceOffAlgorithm-master/bounceOffAlgorithm-master/lib.js
@@ -0,0 +1,28 @@
+function bounceoff (o1,o2) {
+    if (o1.x - o2.x < o2.width/2 + o1.width/2
+      && o2.x - o1.x < o2.width/2 + o1.width/2) {
+    o1.velocityX = o1.velocityX * (-1);
+    o2.velocityX = o2.velocityX * (-1);
+  }
+  if (o1.y - o2.y < o2.height/2 + o1.height/2
+    && o2.y - o1.y < o2.height/2 + o1.height/2){
+    o1.velocityY = o1.velocityY * (-1);
+    o2.velocityY = o2.velocityY * (-1);
+  }
+  }
+
+  function istouching( wrv , cullinan ) 
+  {
+    if (wrv.x - cullinan.x < wrv.width/2 + cullinan.width/2 
+        && cullinan.x - wrv.x < wrv.width/2 + cullinan.width/2 
+        && wrv.y - cullinan.y < wrv.height/2 + cullinan.height/2 
+        && cullinan.y - wrv.y < wrv.height/2 + cullinan.height/2)
+      {
+        return true ;
+      }
+    
+      else {
+       return false ;
+      }
+    
+  }
